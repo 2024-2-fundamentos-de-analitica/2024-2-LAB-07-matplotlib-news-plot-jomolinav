940c5960d2e65b336c45b7d76ab505671ceb3517
jomolinav <jomolinav@unal.edu.co>
2025-01-08 13:45:20 -0500

    upd1Lab7

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,6 +3,24 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
+import pandas as pd
+import matplotlib.pyplot as plt
+import os 
+path = "files/input/news.csv"
+archivo = pd.read_csv(path, index_col=0)
+
+for col in archivo.columns:
+    plt.plot(archivo[col], label=col)
+
+plt.title("Como ven noticias(❁´◡`❁)")    
+plt.legend(loc="upper right")
+
+
+plt.savefig("files/plots/news.png")
+plt.close()
+
+
+
 
 
 def pregunta_01():
